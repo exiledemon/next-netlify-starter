b72e012cfff35b67a41c34cfe44d591eae8df048
exiledemon <91690057+exiledemon@users.noreply.github.com>
2022-02-05 21:38:15 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Nic Ivanov site!" />
+        <Header title="Welcome to Nic Ivanov sitesss!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
